6ecf4c55c3e6ffd1f8b1fb9466127dd1ca4cf43c
mahmoud sobhy <mahmoudsobhy23hf@gmail.com>
2021-05-24 19:39:31 +0200

    add health

--- Assets/scripts/Destroyplayer.cs
+++ Assets/scripts/Destroyplayer.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destroyplayer : MonoBehaviour
 {
 
     public int playerHealth = 3;
+    public Text HealthText;
 
     // Start is called before the first frame update
     void Start()
     {
+        HealthText.text = playerHealth.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            playerHealth -= 1;
+            -- playerHealth;
+            HealthText.text = playerHealth.ToString();
             Debug.Log(playerHealth);
-        }
 
-        if (playerHealth == 0)
-        {
-            Destroy(gameObject);
+            if (playerHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
